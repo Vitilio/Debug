2b227f56fbb7410fc110a0142e5547254391dce2
Vitilio <Vitilio@users.github.com>
2021-02-19 09:19:46 +0100

    Probando el Debug

--- src/main/java/com/mycompany/debug/NewMain.java
+++ src/main/java/com/mycompany/debug/NewMain.java
@@ -4,9 +4,17 @@
  * and open the template in the editor.
  */
 package com.mycompany.debug;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
 
 /**
- *
+ *Escribe un programa en Java que solicite números enteros
+  positivos hasta que el usuario quiera y por cada número
+  introducido deberÃ¡ decirnos si se trata de un número deficiente
+  (Número natural que cumple que la suma de sus divisores propios
+  es menor que el propio número) o no. Utiliza la clase
+  JOptionPane.
+
  * @author daw1
  */
 public class NewMain {
@@ -16,6 +24,30 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        int n;
+	String entrada;
+	String mensaje;
+	int acumulador;
+        
+        entrada=JOptionPane.showInputDialog(null,"Introduzca un numero entero positivo ");
+        n=Integer.parseInt(entrada);
+        while(n>0){
+            acumulador=0;
+            for(int i=1;i<=n/2;i++){
+		if(n%i==0){
+                    acumulador+=i;
+		}
+            }
+        if(acumulador<n){
+			mensaje="El número es deficiente";
+		}
+		else{
+			mensaje="El número NO es deficiente";
+		}
+        JOptionPane.showMessageDialog(null,mensaje);
+	entrada=JOptionPane.showInputDialog(null,"Introduzca un numero entero positivo ");
+	n=Integer.parseInt(entrada);
+	
+        }
+    } 
 }
